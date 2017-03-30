a88439b8e66221f2bf741521d0199e1260fd93ef
Nicolas Payot <nicolas.payot@gmail.com>
2017-03-30 08:53:43 +0200

    feat(app): use EventEmitter to handle input message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import 'material-design-lite';
 
 import { Component } from '@angular/core';
+import { MessageService } from './message/message.service';
 
 @Component({
   selector: 'npa-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private messageService: MessageService) {}
+
+  sendMessage(message) {
+    this.messageService.send(message).subscribe();
+  }
+}
